5c0c81c956b08c072b1ebe9dbda8a7cf85e89bc1
Hunter Underhill <u0977727@hci-moab.hci.utah.edu>
2019-11-22 10:46:04 -0700

    Getting things into and out of VEP

--- aTenuWaitToo.py
+++ aTenuWaitToo.py
@@ -97,6 +97,9 @@ def main():
 					print('SSC output saved at {}'.format(VAR1))
 					parsVAR1 = VAR1[:-2]+'parsed.tsv'
 					postSSCparser(VAR1,parsVAR1)
+					CommonOut = compOut+'/'+bams[:-4]+'.shared.tsv'
+					VepIn = compOut+'/'+bams[:-4]+'.vepIn.tsv'
+					VepOutput = vepOut+'/'+bams[:-4]+'.vep.txt' 
 					print('Parsed SSC saved at {}'.format(parsVAR1+'.gz'))
 				elif bamCount%3 == 0:
 					VAR2 = varOut[:-6]+'ssc.vcf.gz'
@@ -106,170 +109,50 @@ def main():
 					print('Parsed SSC saved at {}'.format(parsVAR2+'.gz'))
 	## Step 3. Using both parsed .vcf files, identify variants that are only present in both.
 					print('Identifying common variants.')
-					commonVars(parsVAR1+'.gz',parsVAR2+'.gz','CommonOut.tsv')
+					commonVars(parsVAR1+'.gz',parsVAR2+'.gz',CommonOut,VepIn)
+					print('Common variants for VEP saved at {}'.format(VepIn))
 
-Now need to pass CommonOut.tsv to VEP and add VEP info to final output. Maybe create second output that generates vepInput file.
-
-
-	## Using the .vcf file created by freebayes and generating total counts and counts for each base at each position - '.counts.tsv'
-	## Using the .vcf file and creating two new files for each variant: 1 file contains snvs at each position - '.pos.snv.vcf', the
-	## other file contains indels at each position - '.pos.indel.vcf'
-#			snvVarOut = compOut+'/'+bams[:-4]+'.pos.snv.vcf'											#fileName of SNVs to be checked against the other duplicate
-#			indelVarOut = compOut+'/'+bams[:-4]+'.pos.indel.vcf'									#fileName of INDELs to be checked against the other duplicate
-#			totalCounts = countOut+'/'+bams[:-4]+'.counts.tsv'											#fileName for data containing read depth information at each position for each base
-#			with open(varOut,'r') as VARS, open(snvVarOut,'w') as snvPos, open(indelVarOut,'w') as indelPos, open(totalCounts,'w') as countsPos:
-#				countsPos.write('Chr\tPos\tTotal\tC\tA\tT\tG\tINDEL\n')
-#				for lines in VARS:
-#					lines = lines.rstrip()
-#					if lines[0] != "#":
-#						info = lines.split()
-#						Chr = info[0]
-#						Pos = info[1]
-#						wt = info[3]
-#						alts = info[4].split(',')
-#						if alts[0] == '.':												#the case where an 'alt' is absent (only WT)
-#							varSearch = info[7].split(';')
-#							for data in varSearch:
-#								if data.startswith('DP='):
-#									dp = data[3:]
-#									if wt == 'C':
-#										countsPos.write('{}\t{}\t{}\t{}\t0\t0\t0\t0\n'.format(Chr,Pos,dp,dp))
-#									elif wt == 'A':
-#										countsPos.write('{}\t{}\t{}\t0\t{}\t0\t0\t0\n'.format(Chr,Pos,dp,dp))
-#									elif wt == 'T':
-#										countsPos.write('{}\t{}\t{}\t0\t0\t{}\t0\t0\n'.format(Chr,Pos,dp,dp))
-#									elif wt == 'G':
-#										countsPos.write('{}\t{}\t{}\t0\t0\t0\t{}\t0\n'.format(Chr,Pos,dp,dp))
-#						else:																			#an 'alt' is present
-#							varSearch = info[7].split(';')
-#							for data in varSearch:
-#								if data.startswith('DP='):						# Total read depth
-#									dp = data[3:]
-#								if data.startswith('RO='):						# WT read depth
-#									dpWT = data[3:]
-#								if data.startswith('AO='):						# Alternate allele read depth
-#									temp = data[3:]
-#									dpAlt = temp.split(',')	
-#								if data.startswith('TYPE='):					# Describes SNV, ins, del
-#									temp = data[5:]
-#									typeAlt = temp.split(',')
-#
-#							ntCounts = 0
-#							baseDict = dict()
-#							for nt in wt:
-#								newPos = str(int(Pos) + ntCounts)
-#								if nt == 'C':
-#									baseDict[newPos] = [str(dp), str(dpWT), str(0), str(0), str(0), str(0)]
-#								elif nt == 'A':
-#									baseDict[newPos] = [str(dp), str(0), str(dpWT), str(0), str(0), str(0)]
-#								elif nt == 'T':
-#									baseDict[newPos] = [str(dp), str(0), str(0), str(dpWT), str(0), str(0)]
-#								elif nt == 'G':
-#									baseDict[newPos] = [str(dp), str(0), str(0), str(0), str(dpWT), str(0)]
-#								ntCounts += 1
-#
-#							varCounts = 0
-#							for var in alts:
-#								if typeAlt[varCounts] == 'snp':
-#									ntCounts = 0
-#									for nt in var:
-#										newPos = str(int(Pos) + ntCounts)
-#										if nt != wt[ntCounts]:
-#											if nt == 'C':
-#												baseDict[newPos][1] = dpAlt[varCounts]
-#											elif nt == 'A':
-#												baseDict[newPos][2] = dpAlt[varCounts]
-#											elif nt == 'T':
-#												baseDict[newPos][3] = dpAlt[varCounts]
-#											elif nt == 'G':
-#												baseDict[newPos][4] = dpAlt[varCounts] 
-#											snvPos.write(Chr+'\t'+newPos+'\t'+wt[ntCounts]+'\t'+nt+'\n')
-#											varCounts += 1
-#											break
-#										else:
-#											ntCounts += 1
-#								elif typeAlt[varCounts] == 'del':
-#									baseDict[Pos][5] = str(int(baseDict[Pos][5])+int(dpAlt[varCounts]))
-#									indelPos.write(Chr+'\t'+newPos+'\t'+wt+'\t'+var+'\n')
-#									varCounts += 1
-#								elif typeAlt[varCounts] == 'ins':
-#									indelPos.write(Chr+'\t'+newPos+'\t'+wt+'\t'+var+'\n')
-#									varCounts += 1
-#
-#							baseDictSorted = sorted(baseDict.items(), key=operator.itemgetter(0))
-#							for values in baseDictSorted:
-#								temp = values[1]
-#								countsPos.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(Chr,values[0],temp[0],temp[1],temp[2],temp[3],temp[4],temp[5]))	
-#			print('Count files written to {}'.format(totalCounts))
-#			print('SNV file written to {}'.format(snvVarOut))
-#			print('INDEL file written to {}'.format(indelVarOut))
-
-#			if bamCount == 1:
-#				compSnvFile1 = snvVarOut
-#				compIndelFile1 = indelVarOut
-#				sharedSnvOut = compOut+'/'+bams[:-4]+'.sharedSnvVars.vcf'
-#				sharedIndelOut = compOut+'/'+bams[:-4]+'.sharedIndelVars.vcf'
-#				bam1counts = totalCounts
-#				bam1Name = bams[:-4]
-#
-#			if bamCount == 2:
-#				print("Comparing SNVs from .bam files.")
-#				with open(compSnvFile1,'r') as snvMod1, open(sharedSnvOut,'w') as shareVar:
-#					for vars1 in snvMod1:
-#						vars1 = vars1.rstrip()
-#						with open(snvVarOut,'r') as snvMod2:
-#							for vars2 in snvMod2:
-#								vars2 = vars2.rstrip()
-#								if vars1 == vars2:
-#									temp = vars1.split()
-#									shareVar.write(temp[0]+'\t'+temp[1]+'\t'+'.'+'\t'+temp[2]+'\t'+temp[3]+'\t'+'.'+'\t'+'PASS'+'\t'+'.\n')
-#				print('The shared SNV file has been written as {}'.format(sharedSnvOut))
-#				with open(compIndelFile1,'r') as indelMod1, open(sharedIndelOut,'w') as shareIndel:
-#					for vars1 in indelMod1:
-#						vars1 = vars1.rstrip()
-#						with open(indelVarOut,'r') as indelMod2:
-#							for vars2 in indelMod2:
-#								vars2 = vars2.rstrip()
-#								if vars1 == vars2:
-#									temp = vars1.split()
-#									shareIndel.write(temp[0]+'\t'+temp[1]+'\t'+'.'+'\t'+temp[2]+'\t'+temp[3]+'\t'+'.'+'\t'+'PASS'+'\t'+'.\n')
-#				print('The shared INDEL file has been written as {}'.format(sharedIndelOut))
-				
 				## vepOpenCmd = 'module load vep'
 				## vepOpenCmd_run = subprocess.run(vepOpenCmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 				## include --humdiv : HumDiv-trained model should be used for evaluating rare alleles at loci potentially involved in 
 				## complex phenotypes, dense mapping of regions identified by genome-wide association studies, and analysis of natural 
 				## selection from sequence data, where even mildly deleterious alleles must be treated as damaging.
 				
-#				if os.stat(sharedSnvOut).st_size > 0:
-#					print('Accessing VEP for potential SNV variants. A while this may take. Patient, you must be.')
-#					vepOutput = vepOut+'/'+bam1Name+'.vep.snv.txt'
-#					vepCmd = 'vep --database -a GRCh37 --port 3337 --sift b --polyphen b --humdiv --check_existing --symbol --force -i {} -o {}'.format(sharedSnvOut,vepOutput)
-#					vepCmd_run = subprocess.run(vepCmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)	
-#					if vepCmd_run.returncode != 0:
-#						print("FAILED! Unable to run VEP for variant classification.")
-#						exit(1)
-#					else:
-#						print("VEP SNV output generated and saved to {}".format(vepOutput))
-#	
-#					vepParsed = vepOutput[:-4]+'.parsed.tsv'
-#					postVepParser(vepOutput,vepParsed)
-#					print('The VEP SNV output has been parsed and saved as a .tsv file at {}'.format(vepParsed))
-#
-#					vepVaf = vepParsed[:-4]+'.VAF.tsv'
-#					postVepAddVaf(bam1counts,vepParsed,vepVaf)
-#					print('The allele counts and VAF have been added to the parsed SNV VEP.tsv file and saved at {}'.format(vepVaf))
-#
-#					adjNoise = finalOut+'/'+bam1Name+'.snv.adj_var.tsv'
-#					postVepVafClass(vepVaf,adjNoise)
-#					with open(adjNoise) as adj:
-#						for i, l in enumerate(adj):
-#							pass
-#						varCounterOut = i
-#					print('Final file with {} adjudicated SNV variants has been created and saved as {}'.format(varCounterOut, adjNoise))
-#				elif os.stat(sharedSnvOut).st_size == 0:
-#						print('There are no shared SNVs between the two .bam files. VEP was not run on SNV data!')
-#	
+					cmdLine = 'gunzip {}'.format(VepIn+'.gz')
+					cmdLine_run = subprocess.run(cmdLine, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+					if cmdLine_run.returncode != 0:
+						print("FAILED! Unable to gunzip {}".format(VepIn+'.gz'))
+						exit(1)
+					if os.stat(VepIn).st_size > 0:
+						print('Accessing VEP for potential variants. A while this may take. Patient, you must be.')
+						vepCmd = 'vep --database -a GRCh37 --port 3337 --sift b --polyphen b --humdiv --check_existing --symbol --force -i {} -o {}'.format(VepIn,VepOutput)
+						vepCmd_run = subprocess.run(vepCmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)	
+						if vepCmd_run.returncode != 0:
+							print("FAILED! Unable to run VEP for variant classification.")
+							exit(1)
+						else:
+							print("VEP output generated and saved to {}".format(VepOutput))
+
+						VepParsed = VepOutput[:-4]+'.parsed.tsv'
+						postVepParser(VepOutput,VepParsed)
+						print('The VEP SNV output has been parsed and saved as a .tsv file at {}'.format(VepParsed))
+
+# everything above is working
+
+						VepVaf = VepParsed[:-4]+'.VAF.tsv'
+						postVepAddVaf(bam1counts,VepParsed,VepVaf)
+						print('The allele counts and VAF have been added to the parsed SNV VEP.tsv file and saved at {}'.format(vepVaf))
+
+						adjNoise = finalOut+'/'+bam1Name+'.snv.adj_var.tsv'
+						postVepVafClass(vepVaf,adjNoise)
+						with open(adjNoise) as adj:
+							for i, l in enumerate(adj):
+								pass
+							varCounterOut = i
+						print('Final file with {} adjudicated SNV variants has been created and saved as {}'.format(varCounterOut, adjNoise))
+					elif os.stat(sharedSnvOut).st_size == 0:
+						print('There are no shared variants between the two .bam files. VEP was not run on SNV data!')
+	
 #				if os.stat(sharedIndelOut).st_size > 0:
 #					print('Accessing VEP for potential INDEL variants. A while this may take. Patient, you must be.')
 #					vepOutput = vepOut+'/'+bam1Name+'.vep.indel.txt'
--- postVep.py
+++ postVep.py
@@ -6,14 +6,49 @@ def postVepParser(inputFile,outputFile):
 	import argparse
 	import os, sys, re
 
-	with open(inputFile) as vepVars:
-		outTemp = []
-		for lines in vepVars:
-			lines = lines.rstrip()
-			if lines[0] != "#":
-				linesSep = lines.split()
-				if linesSep[6] == 'missense_variant':
-					if linesSep[4] == 'ENST00000557334':
+	with open(inputFile,'r') as vepVars1, open(outputFile,'w') as dataOut:
+		dataOut.write('Chr\tPos\tWT\tMUT\tGENE\tcDesc\tpDesc\tIMPACT\tSIFT\tPoly\tClinVar\n')
+		Chr1dup = '0'
+		Pos1dup = '0'
+		mu1dup = '0'
+		for line1 in vepVars1:
+			line1 = line1.rstrip()
+			if line1[0] != "#":
+				line1Sep = line1.split()
+				pos1sep = line1Sep[0].split('_')
+				Chr1 = pos1sep[0]
+				Pos1 = pos1sep[1]
+				var1 = pos1sep[2].split('/')
+				wt1 = var1[0]
+				mu1 = var1[1]
+				Enst1 = line1Sep[4]
+				found1 = re.search(r"ENST0+",Enst1)
+				cENST1 = int(Enst1[len(found1.group(0)):])
+				if (Chr1 == Chr1dup) and (Pos1 == Pos1dup) and (mu1 == mu1dup):
+					continue
+				else:
+					with open(inputFile,'r') as vepVars2:
+						for line2 in vepVars2:
+							line2 = line2.rstrip()
+							if line2[0] != "#":
+								line2Sep = line2.split()
+								pos2sep = line2Sep[0].split('_')
+								Chr2 = pos2sep[0]
+								if Chr1 == Chr2:
+									Pos2 = pos2sep[1]
+									if Pos1 == Pos2:
+										var2 = pos2sep[2].split('/')
+										wt2 = var2[0]
+										mu2 = var2[1]
+										if mu1 == mu2:
+											Enst2 = line2Sep[4]
+											found2 = re.search(r"ENST0+",Enst2)
+											cENST2 = int(Enst2[len(found2.group(0)):])
+											if cENST1 >= cENST2:
+												linesSep = line1Sep
+											elif cENST2 > cENST1:
+												linesSep = line2Sep
+					if (linesSep[6] == 'missense_variant') or (linesSep[6] == 'stop_gained'):
 						pos0sep = linesSep[0].split('_')
 						Chr = pos0sep[0]
 						Pos = pos0sep[1]
@@ -40,11 +75,12 @@ def postVepParser(inputFile,outputFile):
 								for desc in clinVar:
 									clinVarOut = desc
 							if extras.startswith('IMPACT='):
-								impact = extras[7:]	
-						out = Chr+'\t'+Pos+'\t'+wt+'\t'+mu+'\t'+geneName+'\t'+cDesc+'\t'+pDesc+'\t'+impact+'\t'+sift+'\t'+poly+'\t'+clinVarOut
-						outTemp.append(out)
-	outSet = set(outTemp)
-	with open(outputFile,'w') as dataOut:
-		dataOut.write('Chr\tPos\tWT\tMUT\tGENE\tcDesc\tpDesc\tIMPACT\tSIFT\tPoly\tClinVar\n')
-		for lines in outSet:
-			dataOut.write(lines+'\n')
+								impact = extras[7:]
+						if linesSep[6] == 'missense_variant':	
+							outData = Chr+'\t'+Pos+'\t'+wt+'\t'+mu+'\t'+geneName+'\t'+cDesc+'\t'+pDesc+'\t'+impact+'\t'+sift+'\t'+poly+'\t'+clinVarOut
+						elif linesSep[6] == 'stop_gained':
+							outData = Chr+'\t'+Pos+'\t'+wt+'\t'+mu+'\t'+geneName+'\t'+cDesc+'\t'+pDesc+'\t'+impact
+						dataOut.write(outData+'\n')
+						Chr1dup = Chr1
+						Pos1dup = Pos1
+						mu1dup = mu1 
